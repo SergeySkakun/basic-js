1a6d6e2e0f80b2f489d1afba5b6d4d377680fa7a
Sergey Skakun <thexfiles116@gmail.com>
2020-03-19 21:38:01 +0300

    feat: implement first three tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members) === false) {
+    return false;
+  }
+
+  let selectedNames = members.filter((element) => {
+    return (typeof element === 'string') && (element.length !== 0);
+  }).map((elmnt) => {
+    return elmnt.trim();
+  });
+
+  return selectedNames.reduce((acc, element) => {
+    acc.push(element[0].toUpperCase());
+    return acc;
+  }, []).sort().join('');
+
 };
